81df354887da260bdb00eb5b9c3d04c31a152c80
Tim Ryan <id@timryan.org>
2016-11-04 17:07:37 -0400

    Makes hoodlum runnable as Usage: hoodlum input.hdl output.v.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #[macro_use] extern crate hoodlum;
+extern crate regex;
 
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
 use hoodlum::*;
+use regex::Regex;
 
 fn main() {
-    if env::args().len() < 2 {
+    if env::args().len() < 3 {
         println!("Usage: hoodlum input.hdl output.v");
         return;
     }
 
-    let mut f = File::open(env::args().next().unwrap()).unwrap();
+    let args = env::args().collect::<Vec<_>>();
+
+    let mut f = File::open(&args[1]).unwrap();
     let mut code = String::new();
     let _ = f.read_to_string(&mut code);
+
+    let re = Regex::new(r"(?m)//.*").unwrap();
+    let code = re.replace_all(&code, "");
 
     let code = hoodlum::parse_results(&code, hoodlum::hdl_parser::parse_Code(&code));
 
@@ -21,9 +28,9 @@ fn main() {
     println!("Verilog:");
     codelist(&verilog);
 
-    let mut f = File::create("out/test.v").unwrap();
+    let mut f = File::create(&args[2]).unwrap();
     f.write_all(verilog.as_bytes()).unwrap();
 
     println!("");
-    println!("File written as out/test.v");
+    println!("File written as {}", &args[2]);
 }
